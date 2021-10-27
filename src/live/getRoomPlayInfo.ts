5b0e9ae215b71bc09924c0dd2e23ba174efea0d1
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-10-27 13:24:45 +0800

    format

--- src/live/getRoomPlayInfo.ts
+++ src/live/getRoomPlayInfo.ts
@@ -13,21 +13,21 @@ export function getRoomPlayInfo(room_id: number | string): APIRequest<APIRespons
 }
 export interface GetRoomPlayInfo_Data {
     room_id: number,
-        "short_id": number,//display_id
-        "uid": number,
-        "need_p2p": number,
-        "is_hidden": boolean,
-        "is_locked": boolean,
-        "is_portrait": boolean,
-        "live_status": number,
-        "hidden_till": number,
-        "lock_till": number,
-        "encrypted": boolean,
-        "pwd_verified": boolean,
-        "live_time": number,
-        "room_shield": number,
-        "is_sp": number,
-        "special_type": number,
-        "play_url": string,
-        "all_special_types": Array<any>
+    "short_id": number,//display_id
+    "uid": number,
+    "need_p2p": number,
+    "is_hidden": boolean,
+    "is_locked": boolean,
+    "is_portrait": boolean,
+    "live_status": number,
+    "hidden_till": number,
+    "lock_till": number,
+    "encrypted": boolean,
+    "pwd_verified": boolean,
+    "live_time": number,
+    "room_shield": number,
+    "is_sp": number,
+    "special_type": number,
+    "play_url": string,
+    "all_special_types": Array<any>
 }
